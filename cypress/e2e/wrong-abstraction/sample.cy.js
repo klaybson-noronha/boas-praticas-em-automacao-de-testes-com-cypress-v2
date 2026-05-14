e798da81c718c68b88fe1ef16022e7fa7c33796b
Klaybsom Noronha <klaybsom.noronha@natura.net>
2026-05-14 18:41:52 -0300

    Refatora testes para melhorar a legibilidade e remove comando de asserção desnecessário

--- cypress/e2e/wrong-abstraction/sample.cy.js
+++ cypress/e2e/wrong-abstraction/sample.cy.js
@@ -1,32 +1,35 @@
-describe('Wrong abstraction bad practice', () => {
+describe("Wrong abstraction bad practice", () => {
   beforeEach(() => {
-    cy.intercept(
-      'GET',
-      '**/search**'
-    ).as('getStories')
+    cy.intercept("GET", "**/search**").as("getStories");
 
-    cy.visit('https://hackernews-seven.vercel.app')
-    cy.wait('@getStories')
-  })
+    cy.visit("https://hackernews-seven.vercel.app");
+    cy.wait("@getStories");
+  });
 
-  it('uses custom command for assertion just for the sake of reusability', () => {
-    cy.search('cypress')
-    cy.wait('@getStories')
+  it("uses custom command for assertion just for the sake of reusability", () => {
+    cy.search("cypress");
+    cy.wait("@getStories");
 
-    cy.assertResults()
-  })
+    cy.get(".table-row").then((rows) => {
+      expect(rows.length).to.be.at.least(1);
+    });
+  });
 
-  it('uses custom command for assertion just for the sake of reusability', () => {
-    cy.search('selenium')
-    cy.wait('@getStories')
+  it("uses custom command for assertion just for the sake of reusability", () => {
+    cy.search("selenium");
+    cy.wait("@getStories");
 
-    cy.assertResults()
-  })
+    cy.get(".table-row").then((rows) => {
+      expect(rows.length).to.be.at.least(1);
+    });
+  });
 
-  it('uses custom command for assertion just for the sake of reusability', () => {
-    cy.search('playwright')
-    cy.wait('@getStories')
+  it("uses custom command for assertion just for the sake of reusability", () => {
+    cy.search("playwright");
+    cy.wait("@getStories");
 
-    cy.assertResults()
-  })
-})
+    cy.get(".table-row").then((rows) => {
+      expect(rows.length).to.be.at.least(1);
+    });
+  });
+});
