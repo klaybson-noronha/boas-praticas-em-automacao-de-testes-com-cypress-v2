c40c567d83a90bdaddecb55d8fed27a09c1c564d
Klaybsom Noronha <klaybsom.noronha@natura.net>
2026-05-14 18:24:42 -0300

    Atualiza teste para visitar a página de login diretamente e refatora a estrutura do teste CRUD

--- cypress/e2e/dependent-tests/sample.cy.js
+++ cypress/e2e/dependent-tests/sample.cy.js
@@ -1,8 +1,6 @@
 describe('Dependent tests bad practice', () => {
   beforeEach(() => {
-    cy.visit('http://notes-serverless-app.com')
-
-    cy.get('.navbar-nav a:contains(Login)').click()
+    cy.visit('http://notes-serverless-app.com/login')
 
     cy.get('#email').type(Cypress.env('user_email'))
     cy.get('#password').type(Cypress.env('user_password'), { log: false })
@@ -11,28 +9,34 @@ describe('Dependent tests bad practice', () => {
     cy.contains('h1', 'Your Notes').should('be.visible')
   })
 
-  it('creates a note', () => {
+  it('CRUDs a note', () => {
+    //Create a note
     cy.contains('Create a new note').click()
-
     cy.get('#content').type('My note')
     cy.contains('Create').click()
 
-    cy.get('.list-group').should('contain', 'My note')
-  })
-
-  it('edits a note', () => {
+    //Assert the note was created
     cy.get('.list-group').contains('My note').click()
+
+    //Update the note
     cy.get('#content').type(' updated')
     cy.contains('Save').click()
 
+    //Assert the note was updated
     cy.get('.list-group').should('contain', 'My note updated')
     cy.get('.list-group:contains(My note updated)').should('be.visible')
-  })
 
-  it('deletes a note', () => {
+    //Deletes the note
     cy.get('.list-group').contains('My note updated').click()
     cy.contains('Delete').click()
 
+    //Asserts the list has at least one item before asserting the note  deletion
+    //This way, we make sure the list of notes has already rendered 
+    cy.get('.list-group a')
+      .its('length')
+      .should('be.at.least', 1)
+
+    //Assert the note was deleted
     cy.get('.list-group:contains(My note updated)').should('not.exist')
   })
 })
